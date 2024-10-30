c02f597671fe3e5736c0dedec74ce30b9870758b
Javed Akhtar <conn.javed@gmail.com>
2024-10-30 19:54:10 +0530

    file updated

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <div className="text-6xl h-72 font-bold text-yellow-400 flex justify-center items-center bg-[url('./public/Img_bg.jpg')] bg-blend-darken bg-center bg-cover">
-        <div className="bg-clip-text text-transparent bg-gradient-to-r from-indigo-400 via-yellow-50 to-yellow-400">Welcome To The Library</div>
+        <div className="bg-clip-text text-transparent bg-gradient-to-r from-orange-500 via-slate-50 to-green-600">Welcome To The Library</div>
       </div>
       <HomeIntro />
       <div class="h-screen bg-gradient-to-r from-cyan-500 to-yellow-400 flex justify-center items-center font-bold text-4xl text-zinc-50">Work in Progress</div>
